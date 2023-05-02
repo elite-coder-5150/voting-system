4455999b8ba56dbc241c954c2629636fc952f40a
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-05-02 02:11:06 -0500

    defined the footer

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+<footer class="main-footer">
+    <div class="container">
+        <div class="pull-right hidden-xs">
+            <b>Voting system</b>
+        </div>
+        <div class="copyright">
+            &copy; 2023 Kaylios all rights reserved
+        </div>
+    </div>
+</footer>
